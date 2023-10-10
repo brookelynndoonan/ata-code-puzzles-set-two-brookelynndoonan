9e994aeabbc1a79b9c77770e13c1600ef86176ad
brookelynndoonan <brookelynn.doonan@mykenzie.snhu.edu>
2023-10-09 23:45:40 -0600

    completed code puzzles and tests pass accordingly.

--- Greedy/Backpacking/src/main/java/com/kenzie/greedy/backpacking/Backpacking.java
+++ Greedy/Backpacking/src/main/java/com/kenzie/greedy/backpacking/Backpacking.java
@@ -1,6 +1,7 @@
 package com.kenzie.greedy.backpacking;
 
-import java.util.List;
+import javax.lang.model.element.Name;
+import java.util.*;
 
 /**
  * Contains a problem that can be solved using the Greedy Technique.
@@ -10,11 +11,11 @@ public class Backpacking {
     /**
      * Given a list of snacks with their weight, and a given weight limit, return the maximum number
      * of snacks that you could pack without exceeding the weight limit. Assume you only have one of each snack.
-     *
+     * <p>
      * Example: [(snackName, weight)]
      * snacks = [(peanuts, 8), (chips, 3), (protein bar, 9), (berries, 7), (banana, 4), (cookies, 10)]
      * weightLimit = 20
-     *
+     * <p>
      * result = [(chips, 3), (banana, 4), (berries, 7)] -> 3 snacks
      *
      * @param snacks      - the list of snacks from which to identify the maximum snacks to pack
@@ -22,7 +23,19 @@ public class Backpacking {
      * @return the maximum number of snacks we can pack
      */
     public static int getMaximumSnacks(List<Snack> snacks, int weightLimit) {
-        // TODO: Implement an algorithm that utilizes the greedy technique
-        return -1;
+        List<Snack> results = new ArrayList<>();
+        int remainingWeight = weightLimit;
+
+        snacks.sort(Comparator.comparingInt(Snack::getWeight));
+
+        for (Snack snack : snacks) {
+            if (snack.getWeight() <= remainingWeight) {
+                results.add(snack);
+                remainingWeight -= snack.getWeight();
+            } else {
+                break;
+            }
+        }
+        return results.size();
     }
 }
--- Greedy/MeetingRoom/src/main/java/com/kenzie/greedy/meetingroom/MeetingRoom.java
+++ Greedy/MeetingRoom/src/main/java/com/kenzie/greedy/meetingroom/MeetingRoom.java
@@ -1,6 +1,7 @@
 package com.kenzie.greedy.meetingroom;
 
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
 
 /**
  * Contains a problem that can be solved using the Greedy Technique.
@@ -11,18 +12,29 @@ public class MeetingRoom {
      * You have a single meeting room for holding meetings. Given a list of meetings with start and end times,
      * return the maximum number of meetings that the meeting room can accommodate. Assume that all the meetings
      * are for the same day.
-     *
+     * <p>
      * Example: [(meetingName, startTime-endTime)]
      * meetings = [(meeting1, 13-14), (meeting2, 15-16), (meeting3, 8-12), (meeting4, 11-12),
      * (meeting5, 9-11), (meeting6, 14-16)]
-     *
+     * <p>
      * result = [(meeting5, 9-11),(meeting4, 11-12),(meeting1, 13-14),(meeting2, 15-16)] -> 4 meetings
      *
      * @param meetings - the list of possible meetings that can be scheduled
      * @return the maximum number of meetings that can be accommodated
      */
     public static int getMaximumMeetings(List<Meeting> meetings) {
-        // TODO: Implement an algorithm that utilizes the greedy technique
-        return -1;
+
+        ArrayList<Meeting> meetingResult = new ArrayList<>();
+        int schdeuleEndTime = 0;
+
+        meetings.sort(Comparator.comparingInt(Meeting::getEndTime));
+
+        for (Meeting meeting : meetings) {
+            if (meeting.getStartTime() >= schdeuleEndTime) {
+                meetingResult.add(meeting);
+                schdeuleEndTime = meeting.getEndTime();
+            }
+        }
+        return meetingResult.size();
     }
 }
